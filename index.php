4ba378fa8a7f073d408b1866165c6554f18ce676
AnnicaF <annif011098@gmail.com>
2022-11-15 12:34:29 +0100

    Oprettet index.php fil og ændret navn på den gamle index.php

--- index.php
+++ index.php
@@ -4,26 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-
-<form action="signup.inc.php" method="POST">
-
-<input type="text" name="first" placeholder="Firstname">
-<br>
-<input type="text" name="last" placeholder="Larstname">
-<br>
-<input type="text" name="email" placeholder="Email">
-<br>
-<input type="text" name="uid" placeholder="Username">
-<br>
-<input type="password" name="pwd" placeholder="Password">
-<br>
-<button type="submit" name="submit">Sign up</button>
-</form>
     
-
 </body>
 </html>
