2e8913e5a9a0d832325ab3f86da483526386e9c0
AnnicaF <annif011098@gmail.com>
2022-11-17 11:53:15 +0100

    Update er data info

--- signup.inc.php
+++ signup.inc.php
@@ -2,10 +2,13 @@
 include_once 'dbh.inc.php';
 $first = $_POST['first'];
 $last = $_POST['last'];
+$gender = $_POST['gender'];
+$address = $_POST['address'];
+$postal = $_POST['postal'];
+$dof = $_POST['dof'];
+$phone = $_POST['phone'];
 $email = $_POST['email'];
-$uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
-
 
 
 $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) 
