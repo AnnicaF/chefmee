46d35892db7d8eb6c727c773a6b1eb0a22856ab8
AnnicaF <annif011098@gmail.com>
2022-11-17 13:13:26 +0100

    Opdateret database

--- databasecode.sql
+++ databasecode.sql
@@ -1,9 +1,13 @@
-CREATE TABLE users (
-    user_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+CREATE TABLE tester (
+    user_id int(255) AUTO_INCREMENT PRIMARY KEY not null,
     user_first varchar(256) not null,
     user_last varchar(256) not null,
+    user_gender varchar(256) not null,
+    user_addresse varchar(256) not null,
+    user_postal bigint(4) not null,
+    user_dof date(8) not null,
+    user_phone bigint(8) not null,
     user_email varchar(256) not null,
-    user_uid varchar(256) not null,
     user_pwd varchar(256) not null
 );
 
