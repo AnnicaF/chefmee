e695037362519c4dba80222ff04e344f49817b59
shuhaiber <95623080+shuhaiber@users.noreply.github.com>
2022-11-18 13:31:05 +0100

    updata sass

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Madoplevelser i egne omgivelser</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="sass/style.css" type="text/css">
 </head>
 <body>
 <header>
