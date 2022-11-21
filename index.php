3aae7ae6bf1ca0d7c91db5db62acd45b03a9b309
AndyWavey <andythegodfather@gmail.com>
2022-11-21 12:48:18 +0100

    update af forsiden

--- index.php
+++ index.php
@@ -5,30 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Madoplevelser i egne omgivelser</title>
-    <link rel="stylesheet" href="sass/style.css" type="text/css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <header>
     <nav class="nav_bar">
+
     <ul>
-        <img src="" alt="logo">
+    <img class="logo" src="assets/chefmelogo.png" alt="logo">
   <li><a href="">MENUER</a></li>
   <li><a href="">KOKKE</a></li>
   <li><a href="">OM OS</a></li>
+
+    <p class="login_p">Login</p><img class="login_icon" src="assets/icon_user.svg" alt="">
     
-    <button>Login</button>
     </ul>
     </nav>
+    <h1 class="hero_caption">
+                BOOK EN PRIVATE KOK
+            </h1>
+            <h3>
+            Tillad tid til værtskab
+            </h3>
     </header>
    <main>
-    
-        <div class="hero_banner">
-            <div class="hero_caption">
-                BOOK EN PRIVATE KOK
-            </div>
-        <img src="" alt="hero banner">
-        </div>
-    
     </main>
 
 </body>
