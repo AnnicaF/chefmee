ad3dd9fcb6774b1057671aed2583ef8422c65fb9
AnnicaF <annif011098@gmail.com>
2022-12-05 20:13:53 +0100

    Update på form der ikke virkede på grund af et navn der var blevet rettet. Virker igen nu

--- signup.php
+++ signup.php
@@ -3,41 +3,40 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Opret profil</title>
         <link rel="stylesheet" href="styles.css">
     </head>
     
-        <?php require_once 'controllers/authController.php'; ?>
-
-            <?php if(count($errors) > 0): ?>
-                <div class="alert alert-danger">
-                    <?php foreach($errors as $error): ?>
-                        <li> <?php echo $error; ?></li>
-                    <?php endforeach; ?>
-                </div> 
-        <?php endif; ?>
 
     <body>
         <form action="signup.php" method="post" >
 
             <div class="signup_container">
                 
-                
                     <p class="opretprofil">Opret profil</p>
-                
 
                 
-        
                 <div class="google"><button>GOOGLE</button></div>
                 <div class="facebook"><button>FACEBOOK</button></div>
                 
                 <div class="person-oplysninger">Person oplysninger</div>
 
+                <?php require_once 'controllers/authController.php'; ?>
+
+                <?php if(count($errors) > 0): ?>
+                <div class="alert alert-danger">
+                <?php foreach($errors as $error): ?>
+                <li> <?php echo $error; ?></li>
+                <?php endforeach; ?>
+                </div> 
+                <?php endif; ?>
+
                     <div class="fornavn">
                         <label for="firstName">
-                            <input  type="text" name="first" placeholder="Fornavn"> 
+                            <input  type="text" name="firstName" placeholder="Fornavn"> 
                         </label>
                     </div>
                 
