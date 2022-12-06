be60cba118435e58acd0aa1edffc8216a6b82fdd
AndyWavey <andythegodfather@gmail.com>
2022-12-06 13:29:28 +0100

    style nyhedsbrev

--- newsletter/index.php
+++ newsletter/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/chefmee/styles.css">
     <title>Tilmed nyhedsbrev</title>
 </head>
 <body>
@@ -13,15 +13,19 @@
 <h1 class="newsletter__title">Tilmeld dig vores nyhedsbrev</h1>
 
 <form action="subscribe.php">
-    <label for="first_name"> <input type="text" name="first_name">Fornavn 
+    <label for="first_name"> 
+        <input class="nyhedsbrev_input" type="text" name="first_name">Fornavn
 </label><br>
 
-    <label for="last_name"> <input type="text" name="last_name">Efternavn
+    <label for="last_name">
+    <input class="nyhedsbrev_input" type="text" name="first_name">Efternavn
 </label><br>
 
-    <label for="email"> <input type="text" name="email">Email </label><br>
+    <label for="email"> 
+        <input class="nyhedsbrev_input" type="text" name="email">Email 
+    </label><br>
 
-    <input type="submit" name="submit" value="Tilmeld">
+    <input class="nyhedsbrev_btn" type="submit" name="submit" value="Tilmeld">
 </form>
 </div>
 </body>
