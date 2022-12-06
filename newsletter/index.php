edc5dfe10f403114d2569887af3213d5a43b13b4
AndyWavey <andythegodfather@gmail.com>
2022-12-06 13:46:45 +0100

    update

--- newsletter/index.php
+++ newsletter/index.php
@@ -9,6 +9,22 @@
 </head>
 <body>
 
+<header>
+            <nav class="nav_bar-box">
+                <ul>
+                    <a href="index.php"><img class="logo" src="../assets/chefmelogo.png" alt="logo"></a>                    
+                    <li><a href="https://chefme.dk/lej-en-kok">MENUER</a></li>
+                    <li><a href="https://chefme.dk/private-dining">KOKKE</a></li>
+                    <li><a href="https://chefme.dk/om-os">OM OS</a></li>
+
+                    <div class="btn">
+                        <div class="kontakt-call-to-action-2">
+                            <button class="knap-gul" id="button" type="button" name="button">Login</button>
+                        </div>
+                    </div>
+                </ul>
+            </nav>
+        </header>    
 <div class="nyhedsbrev_container">
 <h1 class="newsletter__title">Tilmeld dig vores nyhedsbrev</h1>
 
