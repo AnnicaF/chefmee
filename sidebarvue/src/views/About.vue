13f8568219e5f277017cd86ef0ae299039846263
AndyWavey <andythegodfather@gmail.com>
2022-12-07 12:47:56 +0100

    update af bestillinger

--- sidebarvue/src/views/About.vue
+++ sidebarvue/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
 	<main id="about-page">
-		<h1>About</h1>
-		<p>This is the about page</p>
+		<h1>Dine bestillinger</h1>
+		<p>Her kan du se en oversigt over dine bestillinger</p>
 	</main>
 </template>
