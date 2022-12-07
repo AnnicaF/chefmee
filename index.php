e17cb6f6a96fd2091c18884207d8ac45a63dd917
AnnicaF <annif011098@gmail.com>
2022-12-07 07:31:11 +0100

    Sletter modal

--- index.php
+++ index.php
@@ -17,11 +17,12 @@
                     <li><a href="https://chefme.dk/lej-en-kok">MENUER</a></li>
                     <li><a href="https://chefme.dk/private-dining">KOKKE</a></li>
                     <li><a href="https://chefme.dk/om-os">OM OS</a></li>
+                    
+                    
 
                     <div class="btn">
                         <div class="kontakt-call-to-action-2">
-                            
-                            <button class="knap-gul" id="button" type="button" name="button">Login</button>
+                            <a href="login.php" class="buttonlogin">Login</a>
                             <img class="login_icon"src="assets/icon_user.svg" alt="logind icon">
                         </div>
                     </div>
@@ -86,24 +87,6 @@
             <div class="kontakt-knapper">
                  <div class="kontakt-call-to-action-2"><button class="knap-gul" id="button" type="button" name="button">Nyhedsbrev</button></div>
             </div>
-
-        <!-- <div class="bg-modal">
-            <div class="modal-content">
-                <div id="login_form">
-                    <div class="close">+</div>
-
-                    <form action="" method="post" onsubmit="event.preventDefault(); validateForm()" class="form_container">
-                        <h1 class="logind">Logind</h1>
-                            <input class="modal_input" type="email" name="email" id="email" placeholder="Indtast din email">
-                            <input class="modal_input" type="password" name="password" id="kode" placeholder="Indtast din kode">
-                            <input class="modal_input" type="password" name="cpassword" id="cdkode" placeholder="bekræft din kode">
-                            <input class="modal_input" type="submit" name="submit" value="login" class="form-btn">
-                            <p class="register">Har du ikke en profil? <a href="register.php">Opret profil</a></p>
-                    </form>
-
-                </div>
-            </div>
-        </div> -->
 
         <footer>
         <div class="nyhedsbrev_afmeld">
