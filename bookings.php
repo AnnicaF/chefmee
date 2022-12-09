1674d6f8380b345b9244d1c24150454b59502069
shuhaiber <95623080+shuhaiber@users.noreply.github.com>
2022-12-09 14:35:05 +0100

    Boobking side med info

---
+++ bookings.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bestillinger</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+
+<header>
+            <nav class="nav_bar-box">
+                <ul>
+                    <a href="index.php"><img class="logo" src="assets/chefmelogo.png" alt="logo"></a>                    
+                    <li><a href="https://chefme.dk/lej-en-kok">MENUER</a></li>
+                    <li><a href="https://chefme.dk/private-dining">KOKKE</a></li>
+                    <li><a href="https://chefme.dk/om-os">OM OS</a></li>
+
+                    <div class="btn">
+                        <div class="kontakt-call-to-action-2">
+                            <button class="knap-gul" id="button" type="button" name="button">Logud</button>
+                        </div>
+                    </div>
+                </ul>
+            </nav>
+        </header>   
+
+<body>
+
+
+<div class="bookings_container">
+    <div class="kommende_container">
+        <h3> <strong> Dine kommende begivenheder </strong></h3>
+<p>Dato: 3. Januar 2023 </p>
+<p>Tidspunkt: kl. 18:00 </p>
+<p>Andledning: Fødesdag</p>
+<p>Antal kuverter: 6pax </p>
+<p>Tema: Japansk</p>
+<p>Kok: Anders Jensen </p>
+<br>
+<p>Total: 1.500,00kr</p>
+<br>
+<p>Bemærkninger: 1 person er allergisk for glutten.</p>
+</div>
+       
+        
+        
+        
+<div class="tidligere_container">
+       <h3> <strong> Dine tidligere begivenheder </strong></h3>
+        <p>Dato: 9. December 2022</p>
+        <p>Tidspunkt: kl. 20:00 </p>
+        <p>Andledning: Julefrokost</p>
+        <p>Antal kuverter: 9pax </p>
+        <p>Tema: Dansk</p>
+        <p>Kok: Anders Jensen </p>
+        <br>
+        <p>Total: 6.075,00kr</p>
+        <br>
+        <p>Bemærkninger: ønsker risalamende til desert.</p>
+    </div>
+ </div>
+
+</body>
+</html>
