852f82a38bcee6c81540966266a1f1ea48b013ab
AndyWavey <andythegodfather@gmail.com>
2022-12-09 11:28:42 +0100

    update userprofile

--- userprofile.php
+++ userprofile.php
@@ -10,18 +10,25 @@
 <body>
 
 <div class="profil_container">
-  <div class="profil_menu">test</div>
+  <div class="profil_menu">Menu</div>
   <div class="profil_title">Profil</div>
-  <div class="profil_logud">logud</div>
+  <div class="profil_logud">
+      <div class="kontakt-call-to-action-2">
+                            
+     <a href="login.php" class="buttonlogin">Login</a>
+     <img class="login_icon"src="assets/icon_user.svg" alt="logind icon">
+      </div>
+                        
+  </div>
   <div class="profil_title_2">title2</div>
   <div class="profil_billede">billede</div>
   <div class="profil_retbillede">ret billede</div>
   <div class="profil_oplysninger">profil oplysninger</div>
-  <div class="profil_navn">jens</div>
-  <div class="profil_adresse">kbh slkdksdsdf</div>
+  <div class="profil_navn">Navn</div>
+  <div class="profil_adresse">Adresse</div>
   <div class="profil_telefon">2023393</div>
   <div class="profil_mail">djjd@laskd.com</div>
-  <div class="profil_opdater">opdater</div>
+  <div class="profil_opdater"><Button class="button_opdater">Opdater</Button></div>
 </div>
     
 </body>
