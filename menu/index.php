093601ddc70614176c11b0b423db4328ad3243e2
shuhaiber <95623080+shuhaiber@users.noreply.github.com>
2022-12-12 10:38:40 +0100

    sidebar ny

---
+++ menu/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="da" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Logoer</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles.css">
+  <script src="https://kit.fontawesome.com/ffac4b7b02.js" crossorigin="anonymous"></script>
+  <script src="navbar.js" defer></script>
+</head>
+
+        <header>
+            <nav class="nav_bar-box">
+                <ul>
+                    <a href="./index.php"><img class="logo" src=".../assets/chefmelogo.png" alt="logo"></a>                    
+                    <li><a href="https://chefme.dk/lej-en-kok">MENUER</a></li>
+                    <li><a href="https://chefme.dk/private-dining">KOKKE</a></li>
+                    <li><a href="https://chefme.dk/om-os">OM OS</a></li>
+
+                    <div class="btn">
+                        <div class="kontakt-call-to-action-2">
+                            <button class="knap-gul" id="button" type="button" name="button">Login</button>
+                        </div>
+                    </div>
+                </ul>
+            </nav>
+        </header>  
+
+    
+    <body>
+
+    
+        <main>
+            <div class="sg">
+
+                <div class="sidebar">
+                    <div class="logo-details">
+                        <i class="fa-solid fa-bars"></i>
+                        <div class="logo_name">Profil</div>
+                        <i class='bx bx-menu' id="btn"></i>
+                    </div>
+
+                    <ul class="nav-list">
+                        <li>
+                            <a href="">
+                                <i class="fa-solid fa-user"></i>
+                                <span class="links_name">Oplysninger</span>
+                            </a>
+                            <span class="tooltip">Oplysninger</span>
+                        </li>
+        
+                        <li>
+                            <a href="">
+                                <i class="fa-solid fa-utensils"></i>
+                                <span class="links_name">Bookinger</span>
+                            </a>
+                            <span class="tooltip">Bookinger</span>
+                        </li>
+
+                        <li>
+                            <a href="">
+                                <i class="fa-solid fa-right-from-bracket"></i>
+                                <span class="links_name">Logud</span>
+                            </a>
+                            <span class="tooltip">Logud</span>
+                        </li>
+                    </ul>
+                </div>
+            </div> 
+         </main>
+    </body>    
+</html>
